0a9b3055d90618eee8cba92c4575c4db928033da
윤동훈 <yhsk1218@naver.com>
2017-04-20 19:47:33 +0900

    no message

--- 개천팀/zombieManager.cpp
+++ 개천팀/zombieManager.cpp
@@ -46,11 +46,11 @@ void zombieManager::makeZombie(float x, float y, int type)
 		//기본좀비
 		case 0:
 		{
-			/*
+			
 			_zombie = new zombie1;
 			_zombie->init("zombie1",x,y,5.0f,10.0f,100);
 			_vZombie.push_back(_zombie);
-			*/
+			
 		}
 		break;
 	}
