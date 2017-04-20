cb1c051baa464f278c006dae2059de34203fad51
정종훈 <whdgns0501@gmail.com>
2017-04-20 19:51:57 +0900

    no message

--- 개천팀/mainGame.h
+++ 개천팀/mainGame.h
@@ -1,23 +1,13 @@
 #pragma once
 #include "gameNode.h"
-<<<<<<< HEAD
 #include "PlayerManager.h"
-=======
 #include "zombieManager.h"
-
->>>>>>> 45ae986ace1eec4cd663aa0e67944bcc9733a9d1
 
 class mainGame : public gameNode
 {
 private:
-<<<<<<< HEAD
 	PlayerManager* _pm;
-
-
-=======
-
 	zombieManager* _zm;
->>>>>>> 45ae986ace1eec4cd663aa0e67944bcc9733a9d1
 
 public:
 	virtual HRESULT init(void);
